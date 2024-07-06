32e1eb2a6392e73df5cf15f01d184540536f486e
Gsombra97 <gmmnove@gmail.com>
2024-07-06 14:53:44 -0300

    Cabeçalho criado
    Adicionei um cabeçalho para todas as páginas, além de

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+
+
+
+ 
+  document.addEventListener('DOMContentLoaded', function() {
+ 
+
+  //IMPLEMENTANDO O HEADER EM TUDO
+
+  fetch('../HTML/header.html')
+
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+
+      //BOTÃO TO-TOP
+
+      const scrollToTop = document.getElementById('buttom-top');
+      
+      scrollToTop.addEventListener('click', function() {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      });
+
+    })
+    .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
+
+
+}); //FECHAMENTO do DOMContentLoaded
+
+
+
+
