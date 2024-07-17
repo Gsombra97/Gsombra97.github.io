39ffc1be9658dfbd292cd8df59ca0aa2eef73c8b
Gsombra97 <162511941+Gsombra97@users.noreply.github.com>
2024-07-17 15:56:33 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -13,7 +13,7 @@
 
       //BOTÃO TO-TOP
 
-      const scrollToTop = document.getElementById("buttom-top")
+      const scrollToTop = document.querySelector('#buttom-top')
       
       scrollToTop.addEventListener('click', function() {
         window.scrollTo({
